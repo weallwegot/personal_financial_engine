8e9332cb3e023171bca77f77c4c00258363d6d84
tosin <tosin.sonuyi@gmail.com>
2018-04-13 18:56:56 -0400

    add pint for unit management

--- definitions.py
+++ definitions.py
@@ -1,2 +1,11 @@
 import os
+from pint import UnitRegistry
+
+# load up the registry to be used everywhere
+ureg = UnitRegistry()
+# add currency as a type of unit since it is not part of the default
+ureg.define('usd = [currency]')
+Q_ = ureg.Quantity
+
+
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
