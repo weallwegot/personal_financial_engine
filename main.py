2b4ccdee86387fc30110f6d8d13b48b7002968c4
tosin <tosin.sonuyi@gmail.com>
2018-04-13 19:31:34 -0400

    day simulation loop and $ update logic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+from dateutil import parser
 import os
 import pandas as pd
 import pint
@@ -12,12 +13,13 @@ money_df = pd.read_csv(os.path.join(ROOT_DIR,'data',"test_budget.csv"))
 
 class Transaction(object):
 
-	def __init__(self,f,a,t,d):
+	def __init__(self,f,a,t,d,sd):
 		# change to have the units recognized by pint and the + as a mathematical operation
 		self.frequency = f
 		self.amount = a
 		self.transaction_type = t
 		self.description = d
+		self.sample_date = sd
 		self.parse_attributes()
 
 	def parse_attributes(self):
@@ -31,9 +33,19 @@ class Transaction(object):
 
 		self.frequency = Q_(self.frequency.replace('d','day').replace('w','week').replace(' ','+'))
 
+		self.sample_date = parser.parse(self.sample_date)
+
+	def should_payment_occur_today(self,datetime_object):
+		"""
+		Given a datetime object determine if this transaction
+		would have occurred on a given date
+		function does some math based on the sample date provided
+		and the frequency indicated
+		"""
+		
 
 
-
+		return False
 
 
 rows = money_df.to_records()
@@ -43,9 +55,33 @@ for row in rows:
 	freq = row.Occurrence
 	amt = row.Amount
 	tx_type = row.Type
+	samp_d = row.Sample_Date
 
-	tx = Transaction(f=freq,a=amt,t=tx_type,d=desc)
+	tx = Transaction(f=freq,a=amt,t=tx_type,d=desc,sd=samp_d)
+	# print tx.frequency
 	txs_list.append(tx)
+
+
+DAYS_TO_PROJECT = 60
+START_AMOUNT = Q_(2800,'usd') - Q_(3400,'usd')
+now = datetime.datetime.now()
+
+days = range(DAYS_TO_PROJECT)
+curr_amt = START_AMOUNT
+for day in days:
+	simulated_day = now + Q_(day,'day')
+	for tx in txs_list:
+		if tx.should_payment_occur_today(simulated_day):
+			curr_amt += tx.amount
+
+	print "Day: {}".format(simulated_day)
+	print "Amount: {}".format(curr_amt)
+
+
+
+
+
+
 
 
 
