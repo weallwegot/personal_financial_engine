207f1b01ae306d84af15731106752fe9e4a2937e
tosin <tosin.sonuyi@gmail.com>
2018-07-16 21:26:20 -0400

    create account class and validate inpute attrs

--- main.py
+++ main.py
@@ -10,18 +10,41 @@ from definitions import ROOT_DIR, Q_
 
 
 # read in a dataframe that defines all the recurring money transaction
-money_df = pd.read_csv(os.path.join(ROOT_DIR,'data',"test_budget.csv"))
+# money_df = pd.read_csv(os.path.join(ROOT_DIR,'data',"money_io.csv"))
+money_df = pd.read_csv(os.path.join(ROOT_DIR,'data',"new_job_refinanced_loan.csv"))
+accounts_df = pd.read_csv(os.path.join(ROOT_DIR,'data',"sample_account_info.csv"))
 # print str(money_df)
+
+class Account(object):
+	def __init__(self,name,bal,acct_type,payback_date=None):
+		self.name = name
+		self.balance = bal
+		self.acct_type = acct_type.upper()
+		self.payback_date = payback_date
+		self._validate()
+
+	def _validate(self):
+		if self.acct_type not in ["CHECKING","CREDIT"]:
+			raise ValueError('What is this account type? {}\nMust be checking or credit'
+				.format(self.acct_type))
+		if self.acct_type == "CREDIT":
+			self.balance = -(bal)
+
+			if 28 > int(self.payback_date) > 0:
+				self.payback_date = int(self.payback_date)
+
+
 
 class Transaction(object):
 
-	def __init__(self,f,a,t,d,sd):
+	def __init__(self,f,a,t,d,sd,sc):
 		# change to have the units recognized by pint and the + as a mathematical operation
 		self.frequency = f
 		self.amount = a
 		self.transaction_type = t
 		self.description = d
 		self.sample_date = sd
+		self.source = sc
 		self._parse_attributes()
 
 	def _parse_attributes(self):
@@ -69,27 +92,48 @@ class Transaction(object):
 		return False
 
 
+# Initialize Account Objects
+accts_dict = {}
+acct_rows = accounts_df.to_records()
+for acct in acct_rows:
+	acctname = acct.AccountName
+	bal = acct.Balance
+	acct_type = acct.Type
+	paydate = acct.PayoffDay
+	accts_dict[acctname] = Account(acctname,bal,acct_type,paydate)
+
+
+# Initialize Transaction Objects
 rows = money_df.to_records()
 txs_list = []
+accounts = {}
 for row in rows:
 	desc = row.Description
 	freq = row.Occurrence
 	amt = row.Amount
 	tx_type = row.Type
 	samp_d = row.Sample_Date
+	src = row.Source
 
-	tx = Transaction(f=freq,a=amt,t=tx_type,d=desc,sd=samp_d)
+	tx = Transaction(f=freq,a=amt,t=tx_type,d=desc,sd=samp_d,sc=src)
 	# print tx.frequency
 	txs_list.append(tx)
+	if src in accounts.keys():
+		continue
+	else:	
+		accounts[src] = Account(src)
 
 
-DAYS_TO_PROJECT = 300
-# checkings account vs credit card debt lmao
-START_AMOUNT = Q_(2800,'usd') - Q_(3400,'usd')
+
+# TODO make this a command line argument beloved
+DAYS_TO_PROJECT = 100
 now = datetime.datetime.now()
 tings2plot = []
 days = range(DAYS_TO_PROJECT)
 curr_amt = START_AMOUNT
+
+
+# This the actual simulation running through days
 for day in days:
 	simulated_day = now + Q_(day,'day')
 	for tx in txs_list:
