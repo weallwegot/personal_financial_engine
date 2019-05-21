3e0e606c225674be5f3fd4672151c9de41193fe3
tosin <tosin.sonuyi@gmail.com>
2019-05-20 23:53:33 -0400

    adds js config for invocation url and cognito pool/client/region

--- parallax-template/js/config.js
+++ parallax-template/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_2Q2D82tEw', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5jbtm3t2rhf6jtqc1b8eq7ta5t', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: 'https://5nhlf36qkj.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
