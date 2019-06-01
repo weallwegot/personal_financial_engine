dd3713f83d27bc39bda8745c5faf0daf98ddc1a5
tosin <tosin.sonuyi@gmail.com>
2019-06-01 15:29:07 -0400

    move over budget input html/css/js from ui proto folder

---
+++ parallax-template/js/budget-input.js
@@ -0,0 +1,68 @@
+
+
+const colnames = ["Description","Amount","Occurrence","Type","Sample_Date","Source","Until"]
+
+$(document).ready(function(){
+	$('[data-toggle="tooltip"]').tooltip();
+	var actions = $("table td:last-child").html();
+
+
+	// Append table with add row form on add new button click
+    $(".add-new").click(function(){
+		$(this).attr("disabled", "disabled");
+		var index = $("table tbody tr:last-child").index();
+        var cols = ["name","department","phone"];
+        var row_html = "<tr>"
+        for(idx in colnames){
+            row_html+= `<td><input type="text" class="form-control" name="${colnames[idx]}" id="${colnames[idx]}"</td>`
+        }
+
+        row_html += `<td>${actions}</td>`
+        row_html += '</tr>'
+
+    	$("table").append(row_html);		
+		$("table tbody tr").eq(index + 1).find(".add, .edit").toggle();
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+
+
+	// Add row on add button click
+	$(document).on("click", ".add", function(){
+		var empty = false;
+		var input = $(this).parents("tr").find('input[type="text"]');
+        input.each(function(){
+			if(!$(this).val()){
+				$(this).addClass("error");
+				empty = true;
+			} else{
+                $(this).removeClass("error");
+            }
+		});
+		$(this).parents("tr").find(".error").first().focus();
+		if(!empty){
+			input.each(function(){
+				$(this).parent("td").html($(this).val());
+			});			
+			$(this).parents("tr").find(".add, .edit").toggle();
+			$(".add-new").removeAttr("disabled");
+		}		
+    });
+
+
+
+	// Edit row on edit button click
+	$(document).on("click", ".edit", function(){		
+        $(this).parents("tr").find("td:not(:last-child)").each(function(){
+			$(this).html('<input type="text" class="form-control" value="' + $(this).text() + '">');
+		});		
+		$(this).parents("tr").find(".add, .edit").toggle();
+		$(".add-new").attr("disabled", "disabled");
+    });
+
+
+	// Delete row on delete button click
+	$(document).on("click", ".delete", function(){
+        $(this).parents("tr").remove();
+		$(".add-new").removeAttr("disabled");
+    });
+});
