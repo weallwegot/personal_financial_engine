722a4b1635d3026d130ec82a9e2382e706b70af9
tosin <tosin.sonuyi@gmail.com>
2019-05-31 23:10:22 -0400

    read s3fs, parse data, add total, package into json response

--- lambdas/projected_totals/lambda_handler.py
+++ lambdas/projected_totals/lambda_handler.py
@@ -1,12 +1,15 @@
 
 import boto3
+import csv
 import s3fs
 import json
 import logging
 
 
 my_s3fs = s3fs.S3FileSystem()
-toplevel_dir = "s3://financial-engine-data/users/"
+toplevel_dir = "s3://financial-engine-data"
+forecasted_data_filename = "forecasted-daily-txs.csv"
+COLUMS_WITHOUT_ACCOUNT_TOTALS = ["date", "transactions"]
 
 
 def respond(err, res=None):
@@ -28,29 +31,26 @@ def lambda_handler(event, context):
 
     '''
 
-    operations = {
-        'GET': 'get_method()',
-        'POST': 'post_method()'
-    }
-
     operation = event['httpMethod']
     user_uid = event['requestContext']['authorizer']['claims']['sub']
-    if operation in operations:
-        if operation == 'GET':
 
-            full_path = f"{toplevel_dir}/users/{user_uid}/daily-txs.csv"
-            with my_s3fs.open(full_path, 'r', errors='ignore') as fl:
-                my_file_lines = fl.readlines()
+    full_path = f"{toplevel_dir}/user_data/{user_uid}/{forecasted_data_filename}"
+    with my_s3fs.open(full_path, 'r', errors='ignore') as fh:
+        #my_file_lines = fh.readlines()
+        reader = csv.DictReader(fh)
+        rows = []
 
-            return respond(err=None, res=payload)
+        for row in reader:
+            day_total = sum([float(v) for k, v in row.items()
+                             if k not in COLUMS_WITHOUT_ACCOUNT_TOTALS and not k.endswith('transactions')])
+            row['daily_total'] = day_total
+            row.pop("")
 
-        elif operation == 'POST':
-            print("Event Body {}".format(event['body']))
-            json_data = json.loads(event['body'])
+            rows.append(row)
 
-            return respond(err=None, res=payload)
+    return respond(err=None, res=rows)
 
-        return respond(err=None, res=operations[operation])
 
-    else:
-        return respond(ValueError('Unsupported method "{}"'.format(operation)))
+# with open('event.json') as f:
+#     e = json.load(f)
+# lambda_handler(e, {})
