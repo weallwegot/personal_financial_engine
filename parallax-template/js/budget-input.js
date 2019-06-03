d1f31ee5b735b99c046600d33f6b7690aaa52f43
tosin <tosin.sonuyi@gmail.com>
2019-06-02 22:05:20 -0400

    api call and response log for budget handling retrieval

--- parallax-template/js/budget-input.js
+++ parallax-template/js/budget-input.js
@@ -1,68 +1,151 @@
+/*global WildRydes _config AmazonCognitoIdentity AWSCognito*/
+var WildRydes = window.WildRydes || {};
 
-
-const colnames = ["Description","Amount","Occurrence","Type","Sample_Date","Source","Until"]
-
-$(document).ready(function(){
-	$('[data-toggle="tooltip"]').tooltip();
-	var actions = $("table td:last-child").html();
-
-
-	// Append table with add row form on add new button click
-    $(".add-new").click(function(){
-		$(this).attr("disabled", "disabled");
-		var index = $("table tbody tr:last-child").index();
-        var cols = ["name","department","phone"];
-        var row_html = "<tr>"
-        for(idx in colnames){
-            row_html+= `<td><input type="text" class="form-control" name="${colnames[idx]}" id="${colnames[idx]}"</td>`
-        }
-
-        row_html += `<td>${actions}</td>`
-        row_html += '</tr>'
-
-    	$("table").append(row_html);		
-		$("table tbody tr").eq(index + 1).find(".add, .edit").toggle();
-        $('[data-toggle="tooltip"]').tooltip();
-    });
-
-
-	// Add row on add button click
-	$(document).on("click", ".add", function(){
-		var empty = false;
-		var input = $(this).parents("tr").find('input[type="text"]');
-        input.each(function(){
-			if(!$(this).val()){
-				$(this).addClass("error");
-				empty = true;
-			} else{
-                $(this).removeClass("error");
+(function rideScopeWrapper($) {
+    var authToken;
+    WildRydes.authToken
+        .then(function setAuthToken(token) {
+            if (token) {
+                authToken = token;
+            } else {
+                window.location.href = "/signin.html";
             }
-		});
-		$(this).parents("tr").find(".error").first().focus();
-		if(!empty){
-			input.each(function(){
-				$(this).parent("td").html($(this).val());
-			});			
-			$(this).parents("tr").find(".add, .edit").toggle();
-			$(".add-new").removeAttr("disabled");
-		}		
+        })
+        .catch(function handleTokenError(error) {
+            // take user back to signin if token is not handled
+            alert(error);
+            window.location.href = "/signin.html";
+        });
+
+    var colnames = [
+        "Description",
+        "Amount",
+        "Occurrence",
+        "Type",
+        "Sample_Date",
+        "Source",
+        "Until"
+    ];
+
+    function requestBudgetInfo() {
+        $.ajax({
+            method: "POST",
+            url: _config.api.invokeUrl + "/budget-handling",
+            headers: {
+                Authorization: authToken
+            },
+            data: JSON.stringify({
+                RetrieveOrPlace: "retrieve"
+            }),
+            contentType: "application/json",
+            success: completeRequest,
+            error: function ajaxError(jqXHR, textStatus, errorThrown) {
+                console.error(
+                    "Error requesting ride: ",
+                    textStatus,
+                    ", Details: ",
+                    errorThrown
+                );
+                console.error("Response: ", jqXHR.responseText);
+                alert(
+                    "An error occured when requesting your money trend:\n" +
+                        jqXHR.responseText
+                );
+            }
+        });
+    }
+
+    // complete the request by taking the timeseries data returned and using it to populate the timeseries plot
+    function completeRequest(result) {
+        console.log("Response received from API: ", result);
+    }
+
+    $(document).ready(function() {
+        $('[data-toggle="tooltip"]').tooltip();
+        var actions = $("table td:last-child").html();
+        requestBudgetInfo();
+
+        // Append table with add row form on add new button click
+        $(".add-new").click(function() {
+            $(this).attr("disabled", "disabled");
+            var index = $("table tbody tr:last-child").index();
+            var cols = ["name", "department", "phone"];
+            var row_html = "<tr>";
+            for (idx in colnames) {
+                row_html += `<td><input type="text" class="form-control" name="${
+                    colnames[idx]
+                }" id="${colnames[idx]}"</td>`;
+            }
+
+            row_html += `<td>${actions}</td>`;
+            row_html += "</tr>";
+
+            $("table").append(row_html);
+            $("table tbody tr")
+                .eq(index + 1)
+                .find(".add, .edit")
+                .toggle();
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+
+        // Add row on add button click
+        $(document).on("click", ".add", function() {
+            var empty = false;
+            var input = $(this)
+                .parents("tr")
+                .find('input[type="text"]');
+            input.each(function() {
+                if (!$(this).val()) {
+                    $(this).addClass("error");
+                    empty = true;
+                } else {
+                    $(this).removeClass("error");
+                }
+            });
+            $(this)
+                .parents("tr")
+                .find(".error")
+                .first()
+                .focus();
+            if (!empty) {
+                input.each(function() {
+                    $(this)
+                        .parent("td")
+                        .html($(this).val());
+                });
+                $(this)
+                    .parents("tr")
+                    .find(".add, .edit")
+                    .toggle();
+                $(".add-new").removeAttr("disabled");
+            }
+        });
+
+        // Edit row on edit button click
+        $(document).on("click", ".edit", function() {
+            $(this)
+                .parents("tr")
+                .find("td:not(:last-child)")
+                .each(function() {
+                    $(this).html(
+                        '<input type="text" class="form-control" value="' +
+                            $(this).text() +
+                            '">'
+                    );
+                });
+            $(this)
+                .parents("tr")
+                .find(".add, .edit")
+                .toggle();
+            $(".add-new").attr("disabled", "disabled");
+        });
+
+        // Delete row on delete button click
+        $(document).on("click", ".delete", function() {
+            $(this)
+                .parents("tr")
+                .remove();
+            $(".add-new").removeAttr("disabled");
+        });
     });
-
-
-
-	// Edit row on edit button click
-	$(document).on("click", ".edit", function(){		
-        $(this).parents("tr").find("td:not(:last-child)").each(function(){
-			$(this).html('<input type="text" class="form-control" value="' + $(this).text() + '">');
-		});		
-		$(this).parents("tr").find(".add, .edit").toggle();
-		$(".add-new").attr("disabled", "disabled");
-    });
-
-
-	// Delete row on delete button click
-	$(document).on("click", ".delete", function(){
-        $(this).parents("tr").remove();
-		$(".add-new").removeAttr("disabled");
-    });
-});
+})(jQuery);
