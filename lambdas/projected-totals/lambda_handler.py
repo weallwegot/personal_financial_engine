f0e018f5546529f7f73b2585581eecdfc000b6bb
tosin <tosin.sonuyi@gmail.com>
2019-08-14 23:11:29 -0400

    update the projected total retrieval lambda to get money warning data

--- lambdas/projected-totals/lambda_handler.py
+++ lambdas/projected-totals/lambda_handler.py
@@ -9,6 +9,7 @@ import logging
 my_s3fs = s3fs.S3FileSystem()
 toplevel_dir = "s3://financial-engine-data"
 forecasted_data_filename = "forecasted-daily-txs.csv"
+money_warnings_filename = "money-warnings.csv"
 COLUMS_WITHOUT_ACCOUNT_TOTALS = ["date", "transactions"]
 
 
@@ -24,7 +25,66 @@ def respond(err, res=None):
     }
 
 
-def lambda_handler(event, context):
+def process_forecast_money_reader(csv_dict_reader):
+    """
+    process the csv dict reader that read in the
+    data for the forecasted money
+    """
+
+    rows = []
+
+    for row in csv_dict_reader:
+        day_total = sum([float(v) for k, v in row.items()
+                         if k not in COLUMS_WITHOUT_ACCOUNT_TOTALS and not k.endswith('transactions')])
+        row['daily_total'] = day_total
+        try:
+            row.pop("")
+        except KeyError:
+            pass
+
+        rows.append(row)
+
+    return rows
+
+
+def process_money_warning_reader(csv_dict_reader):
+    """
+    process the csv dict reader that read in the
+    data for the money warnings
+    """
+
+    rows = []
+
+    for row in csv_dict_reader:
+        try:
+            row.pop("")
+        except KeyError:
+            pass
+
+        rows.append(row)
+
+    return rows
+
+
+def retrieve_csv_data(full_path):
+    """
+    retrieve the forecast data
+
+    Arguments:
+        full_path {str} -- path to s3 file for forecast data
+    """
+    with my_s3fs.open(full_path, 'r', errors='ignore') as fh:
+        #my_file_lines = fh.readlines()
+        reader = csv.DictReader(fh)
+        if full_path.endswith(forecasted_data_filename):
+            return process_forecast_money_reader(reader)
+        elif full_path.endswith(money_warnings_filename):
+            return process_money_warning_reader(reader)
+
+    return rows
+
+
+def lambda_handler(event, _context):
     '''Demonstrates a simple HTTP endpoint using API Gateway. You have full
     access to the request and response payload, including headers and
     status code.
@@ -34,26 +94,17 @@ def lambda_handler(event, context):
     operation = event['httpMethod']
     user_uid = event['requestContext']['authorizer']['claims']['sub']
 
-    full_path = f"{toplevel_dir}/user_data/{user_uid}/{forecasted_data_filename}"
-    with my_s3fs.open(full_path, 'r', errors='ignore') as fh:
-        #my_file_lines = fh.readlines()
-        reader = csv.DictReader(fh)
-        rows = []
+    full_path_forecast = f"{toplevel_dir}/user_data/{user_uid}/{forecasted_data_filename}"
+    full_path_money_warnings = f"{toplevel_dir}/user_data/{user_uid}/{money_warnings_filename}"
 
-        for row in reader:
-            day_total = sum([float(v) for k, v in row.items()
-                             if k not in COLUMS_WITHOUT_ACCOUNT_TOTALS and not k.endswith('transactions')])
-            row['daily_total'] = day_total
-            try:
-                row.pop("")
-            except KeyError:
-                pass
+    forecast_data = retrieve_csv_data(full_path_forecast)
+    money_warning_data = retrieve_csv_data(full_path_money_warnings)
 
-            rows.append(row)
+    res_package = {"forecastData": forecast_data, "moneyWarningData": money_warning_data}
 
-    return respond(err=None, res=rows)
+    return respond(err=None, res=res_package)
 
-
-# with open('event.json') as f:
-#     e = json.load(f)
-# lambda_handler(e, {})
+if __name__ == "__main__":
+    with open('event.json') as f:
+        e = json.load(f)
+    # print(lambda_handler(e, {}))
