4d79076d66043f72706b57efc89f466a49246034
tosin <tosin.sonuyi@gmail.com>
2019-08-14 21:08:31 -0400

    adds notes for money warnings and keys

--- lambdas/compute-forecast/definitions.py
+++ lambdas/compute-forecast/definitions.py
@@ -12,11 +12,14 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 CREDIT = "CREDIT"
 CHECKING = "CHECKING"
-VALID_ACCT_TYPES = [CREDIT,CHECKING]
+VALID_ACCT_TYPES = [CREDIT, CHECKING]
+
+DEBT_RATIO = "DEBT/CREDIT RATIO HIGH"
+OVERDRAFT = "CHECKING ACCOUNT OVERDRAFT"
+OVERCREDIT = "BALANCE HIGHER THAN CREDIT LIMIT"
+
+ISSUE_NOTES = {DEBT_RATIO: "FICO recommends keeping a debt to limit ratio of under 30%",
+               OVERDRAFT: "Spent more money than you have in this checking account",
+               OVERCREDIT: "Credit card balance exceeds your limit."}
 
 FOREVER_RECURRING = "recurring payment like forever"
-
-# FOR BOKEH PLOTS
-TOOLTIPS = [
-    ("(x,y)", "($x, $y)"),
-]
